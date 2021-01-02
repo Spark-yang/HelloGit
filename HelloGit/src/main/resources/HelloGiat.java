47a48fe6c1cbacbe01c9b2f8d0d4afdd75ea8ea9
yang <578239292@qq.com>
2021-01-02 10:59:22 +0800

    第二次提交

--- HelloGit/src/main/resources/HelloGiat.java
+++ HelloGit/src/main/resources/HelloGiat.java
@@ -3,5 +3,7 @@ package resources;
 public class HelloGiat {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
